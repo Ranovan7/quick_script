52030742d404f96c981e98ad6bbdccd0d024ace0
Ranovan7 <romyranovan@gmail.com>
2022-01-09 16:50:15 +0700

    update

--- go_example/hello.go
+++ go_example/hello.go
@@ -7,4 +7,17 @@ func main() {
 
     a := []int{21, 22, 23}
     fmt.Println(a)
+
+    var i int
+    i = 4/(4/2)
+    fmt.Println(i)
+
+    i_arr := []int{1,2,3,4}
+    min := i_arr[0]
+    for i := 1; i < len(i_arr); i++ {
+        if min > i_arr[i] {
+            min = i_arr[i]
+        }
+    }
+    fmt.Println(min)
 }
--- ruby_example/hello.rb
+++ ruby_example/hello.rb
@@ -1,1 +1,8 @@
 puts "Hello World"
+
+b = true
+a = b ? 1000.0 : 500.0
+puts a
+
+puts a/3
+puts (a/3)*3
