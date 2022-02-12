066feff91f62160ff9437f7dde171768bd7bd49a
Ranovan7 <romyranovan@gmail.com>
2022-02-12 18:32:22 +0700

    update

--- go_example/itoitonomy.go
+++ go_example/itoitonomy.go
@@ -5,12 +5,13 @@ import "fmt"
 func main() {
     answer := "ABC"
     number := 10
-    somestr, _ := fmt.Printf("answer is = %s, with number = %d", answer, number)
+    somestrstr := "answer is = %s, with number = %d"
+    somestr, _ := fmt.Printf(somestrstr, answer, number)
 
     fmt.Println(somestr)
 
-    arr := []int{1,2,3,4,5}
-    val, isset := arr[7]
-    fmt.Println(val)
-    fmt.Println(isset)
+    // arr := []int{1,2,3,4,5}
+    // val, isset := arr[7]
+    // fmt.Println(val)
+    // fmt.Println(isset)
 }
