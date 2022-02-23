a32f47def82764fe57eb904b4f770e0c11be013c
Ranovan7 <romyranovan@gmail.com>
2022-02-23 13:56:36 +0700

    update gdscript

--- gdscript_example/hello.gd
+++ gdscript_example/hello.gd
@@ -6,4 +6,7 @@ func _init():
     var example = 100 if true else 10
     print(example)
 
+    var parseJSON = JSON.parse('{"example":{"gold":false,"silver":false,"bronze":false}}')
+    print(parseJSON.result)
+
     quit()
